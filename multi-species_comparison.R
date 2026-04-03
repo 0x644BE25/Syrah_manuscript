68199b0142f93be3bebbed31589f90305c94487d
Carolyn Brewster <carolynbrewster@gmail.com>
2026-04-03 15:24:54 -0500

    doneskys

--- multi-species_comparison.R
+++ multi-species_comparison.R
@@ -25,27 +25,38 @@ nPCs <- c('mouse'=5,'planarian'=15,'chick'=15,'human'=10)
 
 minUMI <- 50
 
+nUMIs <- c()
+
 ms_colors <- c('mouse'='#6620bb','planarian'='#496efd','chick'='#bb59bf','human'='#ff585f')
 ms_colors_lt <- c('mouse'='#b390dd','planarian'='#a4b7fe','chick'='#ddacdf','human'='#ffacaf')
 
 set.seed(61687)
 
-# ================= MAKE SEURAT OBJECTS ==============
-
 for (ds in datasets) {
   coords <- read.delim(paste0(ds,'_coordinates.tsv'),header=FALSE,row.names=1)
+  pcs_df <- NULL()
   for (vs in versions) {
     counts <- getCountsFromNpz(paste0(ds,'_',vs,'_sparse_counts.npz'))
+    counts <- counts[,colnames(counts) %in% rownames(coords)]
     counts <- counts[,colSums(counts)>=minUMI]
+    
+    # standard single-cell processing
     seu <- CreateSeuratObject(counts=counts,project=paste0(ds,'_',vs))
-    puck <- as.matrix(coords[Cells(seu),]); colnames(coords) <- c('PUCK_1','PUCK_2')
-    seu[['puck']] <- CreateDimReducObject(embeddings=puck,key='PUCK_',assay='RNA')
     seu <- SCTransform(seu)
     seu <- RunPCA(seu,npcs=nPCs[ds])
     seu <- RunUMAP(seu,dims=1:nPCs[ds],seed.use=61687)
     seu <- FindNeighbors(seu,dims=1:nPCs[ds])
     seu <- FindClusters(seu)
+    
+    # add spatial embedding
+    puck <- as.matrix(coords[Cells(seu),]); colnames(coords) <- c('PUCK_1','PUCK_2')
+    seu[['puck']] <- CreateDimReducObject(embeddings=puck,key='PUCK_',assay='RNA')
+    
+    # save Seurat object
     saveRDS(paste0(ds,'_',vs,'_seurObj.rds'))
+    
+    # collate PC info
+    pcs_df <- rbind(pcs_df,data.frame(version=vs,PC=1:nPCs[ds],stdev=seu[['pca']]@stdev))
   }
+  write.csv(pcs_df,paste0(ds,'_PC_stdevs.csv'))
 }
-
